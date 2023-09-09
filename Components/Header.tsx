2eb060b804c0c1f83d0bd77111de8bdbd2cd0f8e
RahulMishra0722 <rahulmishra@072002@gmail.com>
2023-09-09 08:46:39 +0530

    Initial commit

---
+++ Components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header (){
+    return (
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Click me
+      </button>
+      
+
+    )
+}
